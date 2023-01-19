c5e954b15c658223c34d0eff49f608c8116b6108
Anderson Sipriano <siprianonew@gmail.com>
2023-01-19 10:22:17 -0300

    DocString como comentários

--- m_01_logica_programacao/a_01_comentarios/aula1.py
+++ m_01_logica_programacao/a_01_comentarios/aula1.py
@@ -1,2 +1,17 @@
+"""
+DocString
+escrever o que eu quiser
+mas não é comentário
+
+"""
+
+'''
+DocString
+também posso usar aspas simmples
+
+'''
+
+'''Também posso usar só uma linha'''
+
 # Permite escrever um comentário
 print(123) # escreve na saída
