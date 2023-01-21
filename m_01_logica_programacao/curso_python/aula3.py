c621785511a3608470005e3ca46d455a0aac596d
Anderson Sipriano <siprianonew@gmail.com>
2023-01-20 23:33:48 -0300

    String e scape

---
+++ m_01_logica_programacao/curso_python/aula3.py
@@ -0,0 +1,23 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica(sabe qual é o tipo) / Forte()
+str -> string -> texto
+String são textos que estão dentro de aspas
+"""
+
+print(1234)# Sabe que é um int
+
+# Aspas simples
+print('Anderson')
+
+# Aspas duplas
+print("Anderson")
+
+# Scape
+print("Anderson \"Sipriano\"")
+#nessa forma de escape, se vc inverter as aspas também funciona
+print(1, 'Anderson "Sipriano"')
+
+# r mostra as barras
+print(r"Anderson \"Sipriano\"")
+
